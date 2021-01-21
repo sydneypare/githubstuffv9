9c27d915412370ed2647cd366bc18564b82cf907
Sydney <sydneypare@mhc.ca>
2021-01-20 22:08:55 -0700

    finished the tests for both, going to watch the tests fail and work on the lists

--- StackTest/StackTest.cs
+++ StackTest/StackTest.cs
@@ -86,62 +86,68 @@ namespace StackTest
         //array tests!
 
         #region
-
         [TestMethod]
-        public void List_TestPushPop_String()
+        public void Array_TestPushPop_String()
         {
-            Stack280L<string> q = new Stack280L<string>();
+            Stack280A<string> q = new Stack280A<string>();
             q.Push("First");
             Assert.AreEqual("First", q.Pop());
         }
 
         [TestMethod]
-        public void List_TestPushPeek_String()
+        public void Array_TestPushPeek_String()
         {
-            Stack280L<string> q = new Stack280L<string>();
+            Stack280A<string> q = new Stack280A<string>();
             q.Push("First");
             Assert.AreEqual("First", q.Peek());
         }
 
         [TestMethod]
-        public void List_TestPush3Pop1_String()
+        public void Array_TestPush3Pop1_String()
         {
-            Stack280L<string> q = new Stack280L<string>();
+            Stack280A<string> q = new Stack280A<string>();
+            q.Push("First");
+            q.Push("Second");
+            q.Push("Third");
+            Assert.AreEqual("Second", q.Pop());
+        }
+
+        [TestMethod]
+        public void Array_TestPush3Peek_String()
+        {
+            Stack280A<string> q = new Stack280A<string>();
+            q.Push("number1");
+            q.Push("number2");
+            q.Push("number3");
+            Assert.AreEqual("number1", q.Peek());
+        }
+
+        [TestMethod]
+        public void Array_TestPush1Pop2_String()
+        {
+            Stack280A<string> q = new Stack280A<string>();
             q.Push("First");
             Assert.AreEqual("First", q.Pop());
         }
 
         [TestMethod]
-        public void List_TestPush3Peek_String()
+        public void Array_TestPush2Pop1_Double()
         {
-            Stack280L<string> q = new Stack280L<string>();
-            q.Push("First");
-            Assert.AreEqual("First", q.Peek());
+            Stack280A<double> q = new Stack280A<double>();
+            q.Push(1.0);
+            q.Push(2.0);
+            Assert.AreEqual(1.0, q.Pop());
         }
 
         [TestMethod]
-        public void List_TestPush1Pop2_String()
+        public void Array_TestPush2Pop1_Int()
         {
-            Stack280L<string> q = new Stack280L<string>();
-            q.Push("First");
-            Assert.AreEqual("First", q.Pop());
+            Stack280A<int> q = new Stack280A<int>();
+            q.Push(1);
+            q.Push(2);
+            Assert.AreEqual(1, q.Pop());
         }
 
-        [TestMethod]
-        public void List_TestPush2Pop1_Double()
-        {
-            Stack280L<string> q = new Stack280L<string>();
-            q.Push("First");
-            Assert.AreEqual("First", q.Peek());
-        }
-
-        [TestMethod]
-        public void List_TestPush2Pop1_Int()
-        {
-            Stack280L<string> q = new Stack280L<string>();
-            q.Push("First");
-            Assert.AreEqual("First", q.Peek());
-        }
         #endregion
 
     }
